d1edca0ee1970d6b0e7d2bebd9d9f7b477b2a921
Fernando <fca.apps@gmail.com>
2022-05-03 18:00:47 -0300

    Primeiro Commit

---
+++ Back/src/SALESCAR.API/Controllers/CarrosController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SALESCAR.API.Model;
+
+namespace SALESCAR.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarrosController : ControllerBase
+    {
+
+        public CarrosController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Carro> Get()
+        {
+            return new Carro [] {
+                new Carro () {
+                    CarroId = 1,
+                    Descricao = "Palio",
+                    Marca = "Fiat",
+                    Cor = "Vermelha",
+                    AnoFabricacao = "2010",
+                    AnoModelo = "2010"
+                },
+                new Carro () {
+                    CarroId = 2,
+                    Descricao = "Gol",
+                    Marca = "Volkswagen",
+                    Cor = "Vermelha",
+                    AnoFabricacao = "2010",
+                    AnoModelo = "2010"
+                },
+                new Carro () {
+                    CarroId = 3,
+                    Descricao = "HB20",
+                    Marca = "Hiundai",
+                    Cor = "Vermelha",
+                    AnoFabricacao = "2010",
+                    AnoModelo = "2010"
+                }
+            };
+        }
+    }
+}
---
+++ Back/src/SALESCAR.API/Model/Carro.cs
@@ -0,0 +1,12 @@
+namespace SALESCAR.API.Model
+{
+    public class Carro    
+    {
+        public int CarroId { get; set; }
+        public string Descricao { get; set; }
+        public string Cor { get; set; } 
+        public string AnoModelo { get; set; }
+        public string AnoFabricacao { get; set; }
+        public string Marca { get; set; }
+    }
+}
